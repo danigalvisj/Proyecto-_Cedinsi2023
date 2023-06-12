0ab01e229064a3203cbe1466b37e09d9a6a3f05c
unknown <daniela.galvis.jaimes@gmail.com>
2023-06-11 19:10:06 -0500

    agregar tareas JS

---
+++ tareasJavascript/tarea2.js
@@ -0,0 +1,19 @@
+/*Realizar un programa que permita generar 100 números e imprimir solo los
+números pares*/
+
+
+function generar100numeros (){
+for (let i = 0; i <= 100; i++) 
+    if (i % 2 === 0)
+        console.log(i);
+}
+function ()
+
+//Realizar un programa que permita sumar los 150 primeros números
+
+let suma = 0;
+for (let i = 1; i <= 150; i++) {
+  suma = suma + i;
+}
+console.log("La suma de los 150 primeros números es: " + suma);
+
